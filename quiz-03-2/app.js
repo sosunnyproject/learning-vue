7c7ced694c96b2dae819cc94f9bd94668745ac77
So Sun Park <parksosun1103@gmail.com>
2018-04-15 00:12:48 +0900

    ~03.2 실습 정답확인 후 수정

--- quiz-03-2/app.js
+++ quiz-03-2/app.js
@@ -8,25 +8,20 @@ Vue.component('footer-global-cmp', {
     template: '<p>This is another global child component</p>'
 });
 
+var localcmp = {
+  template: '<p>This is another local child component</p>'
+};
 
 var app = new Vue({
   el: "#app",
   data: {
     message: 'This is a parent component'
-  }
-})
+  },
 
+  // 실습#2 - todo-list 컴포넌트 지역 등록
+  // <p>This is another local child component</p>를 template
 
-// 실습#2 - todo-list 컴포넌트 지역 등록
-// <p>This is another local child component</p>를 template으로 갖는
-// 컴포넌트를 등록해보기
-
-// 지역 컴포넌트 내용
-var localcmp = {
-  template: '<p>This is another local child component</p>'
-};
-new Vue({
-  el: "#app2",
+  // 지역 컴포넌트 등록
   components: {
     'list-local-cmp': localcmp
   }
