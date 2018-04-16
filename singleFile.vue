47efb26e93f9e1c5a93f1fdbf7a3fa952afa551f
= <=>
2018-04-16 18:38:45 +0900

    ~05.2까지 완료

---
+++ singleFile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id='app'>
+    <span>
+      <button v-on:click='clicked'> {{ message }} </button>
+    </span>
+  </div>
+</template>
+
+<script>
+  // test done via copy&pasting onto vueCli/App.vue
+  // QUESTION; why data(), methods : ???
+  export default {
+      name: 'app',
+      data() {
+        return {
+          message: 'click this button'
+      }
+    },
+    methods: {
+      clicked: function() {
+        alert(this.message);
+      }
+    }
+  }
+</script>
+
+<style>
+  span {
+      font-size: 1.2em;
+  }
+</style>
