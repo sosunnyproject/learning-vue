c7e07d46b01c9d7453ff47b8c745088d99eae65f
= <=>
2018-04-24 15:15:26 +0900

    vue-todo app demo

---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <ul>
+      <li>1. 할일</li>
+      <li>2. 할일</li>
+      <li>3. 할일</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        todoItems: []
+      }
+    },
+    created() {
+      if (localStorage.length > 0) {
+        for (var i = 0;i<localStorage.length; i++) {
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
